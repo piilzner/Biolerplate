43cc36302349f195d841af3f35c5fe94501f87f5
Filip Ramstedt <magra@host-193-11-184-88.eduroam.bth.se>
2017-02-16 13:47:18 +0100

    removed $scope log from home controller and added a pbp.json file

--- public/js/controllers/home.controller.js
+++ public/js/controllers/home.controller.js
@@ -1,7 +1,6 @@
 export class HomeController{
     constructor($scope){
         console.log($scope);
-        this.scope = $scope;
         this.scope.title = "Piilzners Boilerplate!";
         this.scope.desc = "Ground zero for your next AngularJS project.";
     }
