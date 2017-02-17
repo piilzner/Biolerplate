6e23bb8ceb4800f67345a1f36dbef428e2bbe38d
Filip Ramstedt <magralol@users.noreply.github.com>
2017-02-17 11:22:17 +0100

    Update home.controller.js

--- public/js/controllers/home.controller.js
+++ public/js/controllers/home.controller.js
@@ -1,7 +1,7 @@
 export class HomeController{
     constructor($scope){
         console.log($scope);
-        this.scope.title = "Piilzners Boilerplate!";
+        this.scope.title = "pbp boilerplates";
         this.scope.desc = "Ground zero for your next AngularJS project.";
     }
 }
